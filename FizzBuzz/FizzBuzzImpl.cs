76269e264a46f5412210762bc768d3d3a88caa38
RawanS <rawansar96@gmail.com>
2017-02-07 15:02:14 -0800

    Completed Implementation and TDD

---
+++ FizzBuzz/FizzBuzzImpl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzImpl
+    {
+        public string Compute(int numToCountTo)
+        {
+            string result = "1";
+            for (int index = 2; index <= numToCountTo; index++)
+            {
+
+                if (CheckDivisibleByBoth(index))
+                {
+                    result += ", Fizz Buzz";
+                }
+                else if (CheckDivisibleByThree(index))
+                {
+                    result += ", Fizz";
+                }
+                else if (CheckDivisibleByFive(index))
+                {
+                    result += ", Buzz";
+                }
+                else
+                {
+                    result += ", " + index;
+                }
+            }
+            return result;
+        }
+
+        private static bool CheckDivisibleByBoth(int index)
+        {
+            return CheckDivisibleByFive(index) && CheckDivisibleByThree(index);
+        }
+
+        private static bool CheckDivisibleByFive(int index)
+        {
+            return index % 5 == 0;
+        }
+
+        private static bool CheckDivisibleByThree(int index)
+        {
+            return index % 3 == 0;
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Shouldly;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class FizzBuzzUnitTest
+    {
+        private FizzBuzzImpl _fb;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _fb = new FizzBuzzImpl();
+        }
+
+        //[Test]
+        //public void FizzBuzzWithOne()
+        //{
+        //    string result = _fb.Compute(1);
+        //    result.ShouldBe("1");
+        //}
+
+        //[Test]
+        //public void FizzBuzzWithThree()
+        //{
+        //    string result = _fb.Compute(3);
+        //    result.ShouldBe("1, 2, Fizz");
+        //}
+
+        //[Test]
+        //public void FizzBuzzWithFive()
+        //{
+        //    string result = _fb.Compute(5);
+        //    result.ShouldBe("1, 2, Fizz, 4, Buzz");
+        //}
+
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(3, ExpectedResult = "1, 2, Fizz")]
+        [TestCase(5, ExpectedResult = "1, 2, Fizz, 4, Buzz")]
+        [TestCase(10, ExpectedResult = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz")]
+        [TestCase(15, ExpectedResult = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz")]
+        public string CountToNWithFizzBuzz(int n)
+        {
+            return _fb.Compute(n);
+        }
+    }
+}
